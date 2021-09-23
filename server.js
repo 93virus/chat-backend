23c43ec1d4bd2e1dbfdb90567c3aa8296dfe979f
93virus <bikasadhikari333@gmail.com>
2021-09-23 12:02:04 +0530

    cors

--- server.js
+++ server.js
@@ -3,17 +3,7 @@ const app = express();
 
 const server = require("http").createServer(app);
 
-const io = require("socket.io")(server,  {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": "https://priceless-dijkstra-4e1b79.netlify.app", //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
-});
+const io = require("socket.io")(server);
 
 app.get("/", (req,res) => {
     res.send("Hello");
